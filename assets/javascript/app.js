52943d9c662bc107e9fd57473c4cff7703611f64
Paul Rose <prose2334@gmail.com>
2018-04-02 23:58:29 -0500

    add notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,44 +7,46 @@
 //when user inputs new word and presses submit, it creates a new button
 
 
-var comicCharacters = ["Hulk", "Batman", "Superman", "Loki", "Joker", "Harley Quinn", "Ant-Man", "Winter Soldier", "Hawkeye", "Black Panther", "Wonder Woman", "Flash"];
+//this is the array of comic character buttons. press one to search giphy for the word inside. new inputs from the user will be pushed to this array
+var comicCharacters = ["Hulk", "Batman", "Superman", "Loki", "Joker", "Harley Quinn", "Ant-Man", "Winter Soldier", "Hawkeye", "Black Panther", "Wonder Woman", "The Flash"];
 
+//when the html has loaded, run the following code/function:
 $(document).ready(function() {
 
+    //for loop, for each string/input in the comicCharacters array, create a button with text equal to the array string/char name
     for (var i = 0; i < comicCharacters.length; i++) {
-        var newButton = $('<button>').html(comicCharacters[i]);
+        var newButton = $('<button>').html(comicCharacters[i]); //new button
         // newButton.attr('type', 'button');
-        newButton.addClass('btn btn-primary btn-sm');
+        newButton.addClass('btn btn-primary btn-sm'); //style the button
         // newButton.attr('id', newButton.text);
-        $('.buttonHolder').append(newButton);
+        $('.buttonHolder').append(newButton); //append the buttons to the html div with buttonHolder class
     }
 
-
+    //when you click on a button, activate this function (make api request):
     $('.btn').on('click', function () {
 
-        var apiKey = "C2vxy7xUanvRx8m3rxQzrM7m200lur0y"
+        var apiKey = "C2vxy7xUanvRx8m3rxQzrM7m200lur0y" //my personal apikey for giphy
         // console.log($(this).text());
-        var searchInput = $(this).html() //this needs to be the button html/text
-        console.log(searchInput);
-        var searchLimit = "10";
+        var searchInput = $(this).html() //grab "this" which is the button you clicked, grab the html from the button clicked, store it as the search input that will be appended to the giphy search url
+        console.log(searchInput); 
+        var searchLimit = "10"; //set the results limit to pull from giphy - 10 images 
         var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + "&q=" + searchInput + "&limit=" + searchLimit;
-        // need url and api and serach params
+        // assemble the url 
 
+        //this is the syntax for an ajax/api call
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(giphyResponse) {
-            console.log(giphyResponse);
+            console.log(giphyResponse); //giphyResponse is the JSON object that contains all the data from the api call
 
-            for (var j = 0; j < giphyResponse.data.length; j++) {
-
+            for (var j = 0; j < giphyResponse.data.length; j++) { //loop over the called data however many times you specified as the search limit (10)
                 // var newGif = $('<div>');
-                var newGifImage = $('<img>');
-                newGifImage.attr('src', giphyResponse.data[j].images.fixed_height.url);
+                var newGifImage = $('<img>'); //create a new image tag
+                newGifImage.attr('src', giphyResponse.data[j].images.fixed_height.url); //add a source attribute and link/properly reference the image url
                 // newGif.append(newGifImage);
-                $(".gifHolder").prepend(newGifImage);
+                $(".gifHolder").prepend(newGifImage); //prepend the new images to the gifHolder div. 
             }
-
 
             // $(".gifHolder").append(giphyResponse.data[0].images.fixed_height.url);
         });
